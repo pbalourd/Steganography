137b1e303732571d3b41a6b7178295bfc9bfba54
Rainbow <pbalourd@yahoo.com>
2021-01-29 14:12:12 +0200

    Check image size added!

--- Steganography/stage3/src/steganography/Main.kt
+++ Steganography/stage3/src/steganography/Main.kt
@@ -3,3 +3,4 @@ package steganography
 fun main() {
     println("Hello World!")
 }
+
--- Steganography/stage3/test/SteganographyTest.kt
+++ Steganography/stage3/test/SteganographyTest.kt
@@ -45,7 +45,7 @@ class SteganographyTest: KotlinTest() {
 
             val message = "Hyperskill steganography program."
             outputString = main.execute(message).toLowerCase().trim()
-            if (!( outputString.contains("message saved")) && outputString.contains("task (hide, show, exit):") ) {
+            if (!( outputString.contains("message saved") && outputString.contains("task (hide, show, exit):") ) ) {
                 return CheckResult(false, "Wrong output after inserting the message.")
             }
 
@@ -68,6 +68,63 @@ class SteganographyTest: KotlinTest() {
                         outputString.contains("task (hide, show, exit):") ) ) {
                 return CheckResult(false, "Wrong output after inserting the input filename.")
             }
+        } catch (e: Exception) {
+            return CheckResult(false, e.message)
+        }
+
+        outputString = main.execute("exit").toLowerCase().trim()
+        if (!outputString.contains("bye!")) {
+            return CheckResult(false, "Wrong output after the \"exit\" command.")
+        }
+
+        if (!main.isFinished) {
+            return CheckResult(false, "Program has not terminated after the \"exit\" command.")
+        }
+
+        return CheckResult(true, "")
+    }
+
+    @DynamicTestingMethod
+    fun smallImageTest(): CheckResult {
+        val main = TestedProgram(mainClass)
+        var outputString = main.start().toLowerCase().trim()
+        if (!outputString.contains("task (hide, show, exit):")) {
+            return CheckResult(false, "Prompt \"Task (hide, show, exit):\" is missing.")
+        }
+
+        outputString = main.execute("hide").toLowerCase().trim()
+        if (!outputString.contains("input image file:")) {
+            return CheckResult(false, "Wrong output after the \"hide\" command.")
+        }
+
+        try {
+            val infile = "test/small.png"
+            val inputFile = File(infile)
+            if (!inputFile.exists()) {
+                return CheckResult(false, "Input test image file doesn't exist. Try reloading the project.")
+            }
+            val inputImage: BufferedImage = ImageIO.read(inputFile)
+            if (imageHash(inputImage) != "c4679ca63e03c377de73ab3aaede91f37a0d50b6") {
+                return CheckResult(false, "Input test image file is corrupted. Try reloading the project.")
+            }
+
+            outputString = main.execute(infile).toLowerCase().trim()
+            if (!outputString.contains("output image file:")) {
+                return CheckResult(false, "Wrong output after inserting the input filename.")
+            }
+
+            val outfile = "test/out2.png"
+            outputString = main.execute(outfile).toLowerCase().trim()
+            if (!outputString.contains("message to hide:")) {
+                return CheckResult(false, "Wrong output after inserting the output filename.")
+            }
+
+            val message = "Hyperskill steganography program."
+            outputString = main.execute(message).toLowerCase().trim()
+            if ( !( outputString.contains("the input image is not large enough to hold this message") && outputString.contains("task (hide, show, exit):") ) ) {
+                return CheckResult(false, "The program should check that the image size is adequate for holding the Bytes array.")
+            }
+
         } catch (e: Exception) {
             return CheckResult(false, e.message)
         }
